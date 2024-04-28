028175c939733816b7c0cb205e22a914ed78351f
kotreshdm <kotreshdm@gmail.com>
2024-04-28 12:04:08 +0530

    removed unused code
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- src/components/admin/Profile.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useAdminContext } from "./Dashboard";
-
-const Profile = () => {
-  const { tab, setTab } = useAdminContext();
-  return <div>Profile - {tab}</div>;
-};
-
-export default Profile;
