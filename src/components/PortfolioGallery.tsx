d8f9975a513eef6d1091b98c550c1bc48d60ad3d
kotreshdm <kotreshdm@gmail.com>
2024-05-01 20:32:41 +0530

    temp remove
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- src/components/PortfolioGallery.tsx
+++ src/components/PortfolioGallery.tsx
@@ -4,14 +4,14 @@ import PortfolioItem from "./PortfolioItem";
 const PortfolioGallery = ({ images, openModal }: any) => {
   return (
     <div id='portfolio' className='flex flex-wrap justify-between'>
-      {images.map((image: any, index: number) => (
+      {/* {images.map((image: any, index: number) => (
         <PortfolioItem
           key={index}
           src={image.src}
           alt={image.alt}
           onClick={() => openModal(index)}
         />
-      ))}
+      ))} */}
     </div>
   );
 };
