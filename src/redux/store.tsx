f2d8e075ae071a356cfea4a19e2ab5c40c171ddf
kotreshdm <kotreshdm@gmail.com>
2024-05-01 18:09:13 +0530

    Removed presiser store
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,5 @@
-"use client";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-
-import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
-import { persistReducer, persistStore } from "redux-persist";
 import themeReducer from "./theme/themeSlice";
 import postReducer from "./posts/postSlice";
 
@@ -12,13 +8,8 @@ const rootReducer = combineReducers({
   theme: themeReducer,
   posts: postReducer,
 });
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: persistedReducer,
-
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
-
-export const persistor = persistStore(store);
--- src/utils/ReactReduxProvider.tsx
+++ src/utils/ReactReduxProvider.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor, store } from "../redux/store";
+import { store } from "../redux/store";
 import { Provider } from "react-redux";
 
 const ReactReduxProvider = ({ children }: any) => {
-  return (
-    <PersistGate persistor={persistor}>
-      <Provider store={store}>{children}</Provider>
-    </PersistGate>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default ReactReduxProvider;
