bab2f3da19931394da032360c72ed1ddbd5d634b
kotreshdm <kotreshdm@gmail.com>
2024-05-04 08:24:26 +0530

    check
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,18 +9,28 @@ import ThemeProvider from "@/utils/ThemeProvider";
 import ApplicationFooter from "@/components/ApplicationFooter";
 import Constants from "@/utils/Constants";
 import Head from "next/head";
+import axios from "axios";
 
 export const metadata: Metadata = {
   title: Constants.SEO.title,
   description: Constants.SEO.description,
   keywords: Constants.SEO.keywords,
 };
+const getPostDetails = async () => {
+  const response = await axios.get(
+    Constants.baseUrl + `${Constants.apiRoutes.getAllCategories}`
+  );
+  return response.data;
+};
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const response: any = await getPostDetails();
+  console.log("response[0].name", response[0].name);
+
   return (
     <html lang='en'>
       <Head>
@@ -38,6 +48,7 @@ export default function RootLayout({
           <ThemeProvider>
             <ApplicationHeader />
             <main className='items-center min-h-screen justify-between dark:bg-gray-800'>
+              "response[0].name": {response[0].name}
               {children}
             </main>
             <ApplicationFooter />
