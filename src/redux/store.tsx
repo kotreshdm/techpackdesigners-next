98a1d84428ec3297a5eede95c5e4dcd9a2737631
kotreshdm <kotreshdm@gmail.com>
2024-05-15 07:01:50 +0530

    Added persister
    Signed-off-by: kotreshdm <kotreshdm@gmail.com>

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,15 +1,20 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice";
+import { persistReducer, persistStore } from "redux-persist";
 import themeReducer from "./theme/themeSlice";
-import postReducer from "./posts/postSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
   theme: themeReducer,
-  posts: postReducer,
 });
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
